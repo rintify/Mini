d31cd45889982c7e903a494d3e8041914c1c0409
ryokutya03 <86511081+ryokutya03@users.noreply.github.com>
2023-08-26 22:02:14 +0900

    ゴールの追加

---
+++ Assets/ryokutya/script/goal.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goal : MonoBehaviour
+{
+    private bool isOn = false;
+    private string playerTag = "Player";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(isOn)
+      {
+            Destroy(this.gameObject);
+       }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == playerTag)
+        {
+            isOn = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == playerTag)
+        {
+            isOn = false;
+        }
+    }
+}
