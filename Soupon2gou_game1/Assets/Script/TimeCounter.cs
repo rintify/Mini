a90e1c52e8e15c3736c9dc1a7f75e948dd3b015b
Soupon2gou <Soupon2gou@outlook.jp>
2023-09-15 15:04:52 +0900

    更新9/15

---
+++ Soupon2gou_game1/Assets/Script/TimeCounter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCounter : MonoBehaviour
+{
+    //カウントダウン
+    private float countdown = 5.0f;
+
+    //時間を表示するText型の変数
+    public Text timeText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //時間をカウントする
+        countdown -= Time.deltaTime;
+
+        //時間を表示する
+        timeText.text = countdown.ToString("f1") + "秒";
+
+        if (countdown <= 0)
+        {
+            timeText.text = "時間になりました！";
+        }
+    }
+}
