4687205125cac7fe483ddc8195fd660d5c52c3f3
ryokutya03 <86511081+ryokutya03@users.noreply.github.com>
2023-10-01 16:28:36 +0900

    BGMの追加と一部スクリプトの変更

--- Assets/ryokutya/script/Timer.cs
+++ Assets/ryokutya/script/Timer.cs
@@ -21,12 +21,12 @@ public class Timer : MonoBehaviour
         if (CountDownTime > 0)
         {
             CountDownTime -= Time.deltaTime;
-            Texttime.text = "じかん:" + CountDownTime.ToString("0.00");
+            Texttime.text = "制限時間:" + CountDownTime.ToString("0.00");
         }
         else
         {
             limit = true;
-            Texttime.text = "じかん:" + CountDownTime.ToString("0.00");
+            Texttime.text = "制限時間:" + CountDownTime.ToString("0.00");
         }
     }
 }
