713a252ddfc8215840c1cbabd6c67a42e92899f0
Soupon2gou <Soupon2gou@outlook.jp>
2023-10-12 23:55:27 +0900

    game3一応完成
    グラフィックのわりにゲーム性がしょぼい

--- Assets/Soupon2gou/Script/game3/PlayerController.cs
+++ Assets/Soupon2gou/Script/game3/PlayerController.cs
@@ -25,7 +25,7 @@ public class PlayerController : MonoBehaviour
 
         if (isStop) return ;
         //毎フレーム回転させる
-        transform.Rotate(new Vector3(0, 0.5f, 0));
+        transform.Rotate(new Vector3(0, 2.0f, 0));
 
         //少しずつ下へ
         //transform.Translate(0, -0.005f, 0);
