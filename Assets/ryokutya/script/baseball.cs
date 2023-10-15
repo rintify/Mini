ccf9b5dd404570781725626bca158e077f8efe2a
ryokutya03 <86511081+ryokutya03@users.noreply.github.com>
2023-10-15 16:21:47 +0900

    操作方法のui（縮小,移動）の実装

--- Assets/ryokutya/script/baseball.cs
+++ Assets/ryokutya/script/baseball.cs
@@ -7,6 +7,7 @@ public class baseball : MonoBehaviour
     public float force;
     private Rigidbody2D rb = null;
     private AudioSource audioSource = null;
+    private float time = 1; 
 
 
     // Start is called before the first frame update
@@ -14,15 +15,23 @@ public class baseball : MonoBehaviour
     {
         audioSource = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody2D>();
+
         Vector3 Force = new Vector3(0, force, 0);
-        rb.AddForce(Force, ForceMode2D.Impulse);
-        audioSource.Play();
+        Invoke("Ball", time);
+        //rb.AddForce(Force, ForceMode2D.Impulse);
+        //audioSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    void Ball()
+    {
+        Vector3 Force = new Vector3(0, force, 0);
+        rb.AddForce(Force, ForceMode2D.Impulse);
+        audioSource.Play();
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
