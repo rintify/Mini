e970a3f11daacbea44caebde94ca4c2d5f0474a3
ryokutya03 <86511081+ryokutya03@users.noreply.github.com>
2023-10-18 15:23:57 +0900

    baseballシーンのバリエーション違いの追加

--- Assets/ryokutya/script/baseball.cs
+++ Assets/ryokutya/script/baseball.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class baseball : MonoBehaviour
 {
     public float force;
+    public bool disapper;
     private Rigidbody2D rb = null;
     private AudioSource audioSource = null;
     private float time = 1;
+    private Renderer ball;
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody2D>();
-
+        ball = this.gameObject.GetComponent<SpriteRenderer>();
         Vector3 Force = new Vector3(0, force, 0);
         Invoke("Ball", time);
         //rb.AddForce(Force, ForceMode2D.Impulse);
@@ -24,7 +26,19 @@ public class baseball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        Vector3 Position = rb.position;
+       if(disapper)
+        {
+             if(Position.y < 2.5)
+            {
+                ball.enabled = false;
+            }
+            if (Position.y < -2)
+            {
+                ball.enabled = true;
+                disapper = false;
+            }
+        }
     }
     void Ball()
     {
