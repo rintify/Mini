359926cb4be5d69b9df6654e53a1cc37eee44b1f
NNinNinn4832 <meijiezhenger@gmail.com>
2023-10-19 18:29:16 +0900

    シーンの追加

--- Assets/NinNin/Script/Player-Dog.cs
+++ Assets/NinNin/Script/Player-Dog.cs
@@ -23,19 +23,20 @@ public class Player : MonoBehaviour
                 this.transform.position += Vector3.left * speed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.D))
         {
             if (this.transform.position.x < 8)
                 this.transform.position += Vector3.right * speed * Time.deltaTime;
         }  
     }
-
+    //リンゴが犬にぶつかったらゲームオーバー
     void OnCollisionEnter2D(Collision2D collision)
   {
             if(collision.gameObject.CompareTag("Apple"))
             { 
                 gameOverUI.SetActive(true);
                 isGameOver = true;
+                Debug.Log("false");
             }
   }
 }
