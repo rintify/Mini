3bc1e8b5e8632b4ae5ab84fd8a1b99717a39cc77
Xenon0R <sss0104x@gmail.com>
2023-10-19 13:40:10 +0900

    一時的な全体像追加

---
+++ Assets/Xenon0R/Materials/Scripts/TImeCounter.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+ 
+public class SampleScene : MonoBehaviour
+{
+    //カウントダウン
+    public float countdown = 7.00f;
+ 
+    //時間を表示するText型の変数
+    public TMP_Text timeText;
+ 
+    //ポーズしているかどうか
+    private bool isPose = false;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        //クリックされたとき
+        if(Input.GetMouseButtonDown(0))
+        {
+            //countdownが0を切っていないとき
+            if(countdown >= 0)
+            {
+                //ポーズ中にクリックされたとき
+                if(isPose)
+                {
+                     //ポーズ状態を解除する
+                     isPose = false;
+                 }
+                //進行中にクリックされたとき
+                else    {
+                     //ポーズ状態にする
+                     isPose = true;
+                }
+            }
+        }
+ 
+        //ポーズ中かどうか
+        if (isPose)
+        {
+            //カウントダウンしない
+            return;
+        }
+ 
+        //時間をカウントする
+        countdown -= Time.deltaTime;
+ 
+        //時間を表示する
+        timeText.text = countdown.ToString("f2");
+ 
+        //countdownが0以下になったとき
+        if (countdown <= 0)
+        {
+            timeText.text = "Time Over";
+        }
+    }
+}
