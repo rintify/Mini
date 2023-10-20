4c54380331bee37d3fb8996aafb78213f7a83b11
Soupon2gou <Soupon2gou@outlook.jp>
2023-10-20 12:38:06 +0900

    GameTransitionの準備段階

--- Assets/Common/GameTransition.cs
+++ Assets/Common/GameTransition.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameTransition : MonoBehaviour
 {
@@ -14,18 +15,31 @@ public class GameTransition : MonoBehaviour
 
     //次のゲームに遷移する時にコモンから呼ばれる
     public void GameToGame(string nextSceneName){
-        SceneManager.LoadScene(nextSceneName);
+        IEnumerator GtoG()
+        {
+            /*GameObject Im = transform.Find("Canvas/Image").gameObject;
+            Im.GetComponent<Image>().color = new Color(0.8f, 0.3f, 0.1f, 255.0f);
+            var tansitionAnim = img.GetComponent<Animator>();
+            tansitionAnim.SetTrigger("out2");*/
+            yield return new WaitForSeconds(1);
+            SceneManager.LoadScene(nextSceneName);
+            /*yield return new WaitForSeconds(1);
+            Im.GetComponent<Image>().color = new Color(0.8f, 0.3f, 0.1f, 0.0f);
+            tansitionAnim.SetTrigger("in2");*/
+        }
+        StartCoroutine(GtoG());
+        
     }
 
     //リザルト画面に遷移する時にコモンから呼ばれる
     public void GameToResult(string resultSceneName){
         IEnumerator Fin()
         {
-            var tansitionAnim1 = img.GetComponent<Animator>();
-            tansitionAnim1.SetTrigger("End");
+            var TansitionAnimResult = img.GetComponent<Animator>();
+            TansitionAnimResult.SetTrigger("out");
             yield return new WaitForSeconds(1);
             SceneManager.LoadScene(resultSceneName);
-            tansitionAnim1.SetTrigger("Start");
+            TansitionAnimResult.SetTrigger("in");
         }
         StartCoroutine(Fin());
     }
