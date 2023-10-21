3c8bea6c6744a324105d56d617e945b445768576
rintify <87462604+rintify@users.noreply.github.com>
2023-10-21 14:28:06 +0900

    バグ修正ing

--- Assets/Common/TestTitle.cs
+++ Assets/Common/TestTitle.cs
@@ -8,35 +8,31 @@ public class TestTitle : MonoBehaviour
 {
     private bool firstPush=false;
     private bool can_do = false;
-    MathEX.Virgin starterSet;
 
     private void Start()
     {
         //allow_startを3.5秒後に呼び出す
         Invoke(nameof(allow_start), 4.5f);
-        starterSet = new(()=>{
-            Debug.Log("Press Start");
-       
-            if (!firstPush && can_do)
-            {
-                Debug.Log("Go Next Scene!");
-                //ここに次のシーンに行く命令を書く
-                Common.StartGames("Alice","Japanese");
-                firstPush = true;
-            }
-        });
     }
 
     private void Update() {
         if(Input.GetKeyDown(KeyCode.Space)){
-            starterSet.Break();
+            PressStart();
         }
     }
 
     //スタートボタンを押されたら呼ばれる
     public void PressStart()
     {
-        starterSet.Break();
+        Debug.Log("Press Start");
+       
+        if (!firstPush && can_do)
+        {
+            Debug.Log("Go Next Scene!");
+            //ここに次のシーンに行く命令を書く
+            Common.StartGames("Alice","Japanese");
+            firstPush = true;
+        }
     }
 
     public void allow_start()
