c20f13a2756d95a291b51b5e0d3a716a85bf0d09
Soupon2gou <87348740+Soupon2gou@users.noreply.github.com>
2023-10-27 14:02:14 +0900

    タイトルのUIのタイミングで出るようにする

--- Assets/Common/TestTitle.cs
+++ Assets/Common/TestTitle.cs
@@ -10,7 +10,7 @@ public class TestTitle : MonoBehaviour
     private bool firstPush=false;
     private bool can_do = false;
 
-    public string username;
+    public string username="ゲスト";
     private string language="Japanese";
 
 
@@ -18,14 +18,40 @@ public class TestTitle : MonoBehaviour
     private void Start()
     {
         
-        GameObject obj = GameObject.Find("入力確認"); //オブジェクトを探す
-        IM = obj.GetComponent<InputFieldManager>(); //付いているスクリプトを取得
         //allow_startを3.5秒後に呼び出す
         Invoke(nameof(allow_start), 4.5f);
+
+        GameObject obj1 = GameObject.Find("input_name"); //オブジェクトを探す
+        GameObject obj2 = GameObject.Find("InputField"); //オブジェクトを探す
+        GameObject obj3 = GameObject.Find("入力確認"); //オブジェクトを探す
+        GameObject obj4 = GameObject.Find("日本語"); //オブジェクトを探す
+        GameObject obj5 = GameObject.Find("言語選択"); //オブジェクトを探す
+        GameObject obj6 = GameObject.Find("英語"); //オブジェクトを探す
+        GameObject obj7 = GameObject.Find("InputField/Text Area/na"); //オブジェクトを探す
+        GameObject obj8 = GameObject.Find("日本語/jp"); //オブジェクトを探す
+        GameObject obj9 = GameObject.Find("英語/en"); //オブジェクトを探す
+        GameObject obj10 = GameObject.Find("ランキング"); //オブジェクトを探す
+        GameObject obj11 = GameObject.Find("ランキング/ran"); //オブジェクトを探す
+
+        obj1.GetComponent<CanvasRenderer>().SetAlpha(0);
+        obj2.GetComponent<CanvasRenderer>().SetAlpha(0);
+        obj3.GetComponent<CanvasRenderer>().SetAlpha(0);
+        obj4.GetComponent<CanvasRenderer>().SetAlpha(0);
+        obj5.GetComponent<CanvasRenderer>().SetAlpha(0);
+        obj6.GetComponent<CanvasRenderer>().SetAlpha(0);
+        obj7.GetComponent<CanvasRenderer>().SetAlpha(0);
+        obj8.GetComponent<CanvasRenderer>().SetAlpha(0);
+        obj9.GetComponent<CanvasRenderer>().SetAlpha(0);
+        obj10.GetComponent<CanvasRenderer>().SetAlpha(0);
+        obj11.GetComponent<CanvasRenderer>().SetAlpha(0);
+
+        Debug.Log("transparent");
+        IM = obj3.GetComponent<InputFieldManager>(); //付いているスクリプトを取得
+        Invoke(nameof(non_transparent), 4.5f);
     }
 
     private void Update() {
-        username = IM.name;
+        
         if (Input.GetKeyDown(KeyCode.Space)){
             PressStart();
         }
@@ -50,6 +76,7 @@ public void ChangeToJapanese()
        
         if (!firstPush && can_do)
         {
+            username = IM.name;
             Debug.Log("Go Next Scene!");
             //ここに次のシーンに行く命令を書く
             Common.StartGames(username,language);
@@ -61,5 +88,34 @@ public void ChangeToJapanese()
     public void allow_start()
     {
         can_do = true;
+    }
+
+    public void non_transparent(){
+        GameObject obj1 = GameObject.Find("input_name"); //オブジェクトを探す
+        GameObject obj2 = GameObject.Find("InputField"); //オブジェクトを探す
+        GameObject obj3 = GameObject.Find("入力確認"); //オブジェクトを探す
+        GameObject obj4 = GameObject.Find("日本語"); //オブジェクトを探す
+        GameObject obj5 = GameObject.Find("言語選択"); //オブジェクトを探す
+        GameObject obj6 = GameObject.Find("英語"); //オブジェクトを探す
+        GameObject obj7 = GameObject.Find("InputField/Text Area/na"); //オブジェクトを探す
+        GameObject obj8 = GameObject.Find("日本語/jp"); //オブジェクトを探す
+        GameObject obj9 = GameObject.Find("英語/en"); //オブジェクトを探す
+        GameObject obj10 = GameObject.Find("ランキング"); //オブジェクトを探す
+        GameObject obj11 = GameObject.Find("ランキング/ran"); //オブジェクトを探す
+
+        obj1.GetComponent<CanvasRenderer>().SetAlpha(1);
+        obj2.GetComponent<CanvasRenderer>().SetAlpha(1);
+        obj3.GetComponent<CanvasRenderer>().SetAlpha(1);
+        obj4.GetComponent<CanvasRenderer>().SetAlpha(1);
+        obj5.GetComponent<CanvasRenderer>().SetAlpha(1);
+        obj6.GetComponent<CanvasRenderer>().SetAlpha(1);
+        obj7.GetComponent<CanvasRenderer>().SetAlpha(1);
+        obj8.GetComponent<CanvasRenderer>().SetAlpha(1);
+        obj9.GetComponent<CanvasRenderer>().SetAlpha(1);
+        obj10.GetComponent<CanvasRenderer>().SetAlpha(1);
+        obj11.GetComponent<CanvasRenderer>().SetAlpha(1);
+
+
+        Debug.Log("nontransparent");
     }
 }
