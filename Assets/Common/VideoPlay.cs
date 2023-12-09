7cf35f2efa9898ed08cadd79c4508c862bc70e07
Soupon2gou <87348740+Soupon2gou@users.noreply.github.com>
2023-12-10 01:52:56 +0900

    a

---
+++ Assets/Common/VideoPlay.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlay : MonoBehaviour
+{
+    public UnityEngine.Video.VideoPlayer video;
+
+    void Start()
+    {
+        video.url = "https://cas-mini.s3.ap-northeast-1.amazonaws.com/cas_background.mp4";
+        video.prepareCompleted += PrepareCompleted;
+        video.errorReceived += ErrorReceived;
+    }
+
+    // エラー発生時に呼ばれる
+    void ErrorReceived(UnityEngine.Video.VideoPlayer vp, string message)
+    {
+        Debug.LogWarning($"動画の読み込みに失敗しました. message:{message}");
+        vp.errorReceived -= ErrorReceived;
+        vp.prepareCompleted -= PrepareCompleted;
+        // エラー時処理
+    }
+
+    // 動画の読み込みが完了したら呼ばれる
+    void PrepareCompleted(UnityEngine.Video.VideoPlayer vp)
+    {
+        Debug.Log("動画ロード完了");
+        vp.prepareCompleted -= PrepareCompleted;
+        vp.Play();
+    }
+}
